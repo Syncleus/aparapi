2a5d73097a9e62f79d0ef23a2eb19954e68d8dc2
Gary Frost <frost.gary@gmail.com>
2011-11-07 19:18:33 +0000

    Added test cases for access to elements of multi dimensional arrays.
    http://code.google.com/p/aparapi/issues/detail?id=10
    
    We need to test both accesses and assignments.

--- test/codegen/src/java/com/amd/aparapi/test/Assign2DIntArray.java
+++ test/codegen/src/java/com/amd/aparapi/test/Assign2DIntArray.java
@@ -1,10 +1,10 @@
 package com.amd.aparapi.test;
 
-public class Access2DIntArray{
+public class Assign2DIntArray{
    int[][] ints = new int[1024][];
 
    public void run() {
-      int value= ints[0][0];
+      ints[0][0] = 1;
    }
 }
 /**{Throws{ClassParseException}Throws}**/
