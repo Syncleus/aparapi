fc1a05edbd9723fd876d5db812033bce6589226f
Gary Frost <frost.gary@gmail.com>
2012-02-20 21:02:44 +0000

    Reported as issue #35 but does not seem to fail.

---
+++ test/codegen/src/java/com/amd/aparapi/test/ArrayTortureIssue35.java
@@ -0,0 +1,36 @@
+package com.amd.aparapi.test;
+
+public class ArrayTortureIssue35{
+   int[] a = new int[1];
+   int[] b = new int[1];
+
+   public void run() {
+       a[b[0]++] = 1;
+   }
+}
+/**{OpenCL{
+typedef struct This_s{
+   __global int *a;
+   __global int *b;
+   int passid;
+}This;
+int get_pass_id(This *this){
+   return this->passid;
+   }
+
+__kernel void run(
+   __global int *a,
+   __global int *b,
+   int passid
+){
+   This thisStruct;
+   This* this=&thisStruct;
+   this->a = a;
+   this->b = b;
+   this->passid = passid;
+   {
+      this->a[this->b[0]++] = 1;
+      return;
+   }
+}
+}OpenCL}**/
