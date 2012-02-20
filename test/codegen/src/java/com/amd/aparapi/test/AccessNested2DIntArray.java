6213fa8ba872f78a38722f241fe4362aaaf45be7
Gary Frost <frost.gary@gmail.com>
2012-02-20 21:45:35 +0000

    Added to evaluate 2D array accesses which fail at codegen time.

---
+++ test/codegen/src/java/com/amd/aparapi/test/AccessNested2DIntArray.java
@@ -0,0 +1,12 @@
+package com.amd.aparapi.test;
+
+public class AccessNested2DIntArray{
+   int[] indices = new int[1024];
+   int[][] ints = new int[1024][];
+
+   public void run() {
+      int value= ints[indices[0]][0];
+   }
+  
+}
+/**{Throws{ClassParseException}Throws}**/
