f8492172de010b8d378187e270bfe80fe4a25a5b
Gary Frost <frost.gary@gmail.com>
2012-02-24 22:29:10 +0000

    New profiling mechanism now works with explicit buffer transfers.

--- examples/nbody/nbody.bat
+++ examples/nbody/nbody.bat
@@ -3,6 +3,7 @@
 java ^
   -Djava.library.path=..\..\com.amd.aparapi.jni\dist;jogamp ^
   -Dcom.amd.aparapi.executionMode=%1 ^
+  -Dcom.amd.aparapi.enableProfiling=true ^
   -Dbodies=%2 ^
   -Dheight=600 ^
   -Dwidth=600 ^
