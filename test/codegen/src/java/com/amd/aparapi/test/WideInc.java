c044e08bf7cbde8c7d54c89118c52d4954a89929
Gary Frost <frost.gary@gmail.com>
2012-04-10 20:16:38 +0000

    Adding wideInc test

---
+++ test/codegen/src/java/com/amd/aparapi/test/WideInc.java
@@ -0,0 +1,35 @@
+package com.amd.aparapi.test;
+
+public class WideInc{
+
+   public void run() {
+      int value = 0;
+      value+=128;
+   }
+}
+
+/**{OpenCL{
+typedef struct This_s{
+   
+   int passid;
+}This;
+int get_pass_id(This *this){
+   return this->passid;
+   }
+
+void func(This *this, int _arg){
+   return;
+}
+__kernel void run(
+   int passid
+){
+   This thisStruct;
+   This* this=&thisStruct;
+   this->passid = passid;
+   {
+      int value;
+      value+=128;
+      return;
+   }
+}
+}OpenCL}**/
