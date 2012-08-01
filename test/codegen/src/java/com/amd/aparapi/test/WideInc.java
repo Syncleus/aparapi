576e50e8752c6fe61bf1ee9c5220ca711cf37090
Gary Frost <frost.gary@gmail.com>
2012-08-01 00:00:32 +0000

    Incorrect expected output

--- test/codegen/src/java/com/amd/aparapi/test/WideInc.java
+++ test/codegen/src/java/com/amd/aparapi/test/WideInc.java
@@ -10,16 +10,12 @@ public class WideInc{
 
 /**{OpenCL{
 typedef struct This_s{
-   
    int passid;
 }This;
 int get_pass_id(This *this){
    return this->passid;
    }
 
-void func(This *this, int _arg){
-   return;
-}
 __kernel void run(
    int passid
 ){
@@ -27,7 +23,7 @@ __kernel void run(
    This* this=&thisStruct;
    this->passid = passid;
    {
-      int value;
+      int value = 0;
       value+=128;
       return;
    }
