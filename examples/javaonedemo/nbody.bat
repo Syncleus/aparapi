9ccbc2e95c6d033547632cc6991909296ab7cb03
Gary Frost <frost.gary@gmail.com>
2012-10-02 04:21:20 +0000

    Added JavaDemoCode.  Used in community keynote. Note the JOGAMP dependency and licenses in the jogamp subdir.

---
+++ examples/javaonedemo/nbody.bat
@@ -0,0 +1,13 @@
+@echo off
+
+java ^
+  -Djava.library.path=..\..\com.amd.aparapi.jni\dist;jogamp ^
+  -Dcom.amd.aparapi.executionMode=%1 ^
+  -Dcom.amd.aparapi.enableProfiling=false ^
+  -Dbodies=%2 ^
+  -Dheight=600 ^
+  -Dwidth=600 ^
+  -classpath jogamp\gluegen-rt.jar;jogamp\jogl.all.jar;..\..\com.amd.aparapi\dist\aparapi.jar;javaonedemo.jar ^
+  com.amd.aparapi.examples.javaonedemo.Main 
+
+
