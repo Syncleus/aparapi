8fb926ba87883c62077a820613128cbdae540478
Gary Frost <frost.gary@gmail.com>
2012-10-23 20:57:39 +0000

    This was incorrectly tagged as expecting an error.

--- test/codegen/src/java/com/amd/aparapi/test/ClassHasStaticMethodSimple.java
+++ test/codegen/src/java/com/amd/aparapi/test/ClassHasStaticMethodSimple.java
@@ -11,4 +11,26 @@ public class ClassHasStaticMethodSimple{
 
    }
 }
-/**{Throws{ClassParseException}Throws}**/
+
+/**{OpenCL{
+typedef struct This_s{
+   int passid;
+}This;
+int get_pass_id(This *this){
+   return this->passid;
+}
+void com_amd_aparapi_test_ClassHasStaticMethodSimple__staticMethod(){
+   return;
+}
+__kernel void run(
+   int passid
+){
+   This thisStruct;
+   This* this=&thisStruct;
+   this->passid = passid;
+   {
+      com_amd_aparapi_test_ClassHasStaticMethodSimple__staticMethod();
+      return;
+   }
+}
+}OpenCL}**/
