18445743133c46462105f4ad55d2c46ff0210231
Gary Frost <frost.gary@gmail.com>
2012-12-17 16:54:33 +0000

    Added by gary

--- examples/javaonedemo/life.sh
+++ examples/javaonedemo/life.sh
@@ -1,7 +1,4 @@
 java \
-  -Djava.library.path=../../com.amd.aparapi.jni/dist \
-  -Dcom.amd.aparapi.executionMode=$1 \
-  -Dheight=600 \
-  -Dwidth=600 \
-  -classpath ../../com.amd.aparapi/dist/aparapi.jar:javaonedemo.jar \
-  com.amd.aparapi.examples.javaonedemo.Life
+   -Djava.library.path=../../com.amd.aparapi.jni/dist \
+   -classpath ../../com.amd.aparapi/dist/aparapi.jar:javaonedemo.jar \
+   com.amd.aparapi.examples.javaonedemo.Life
