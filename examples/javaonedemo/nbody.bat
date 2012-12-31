af59840baf13f085fb9695e5b13a3f69230af08a
Gary Frost <frost.gary@gmail.com>
2012-12-31 17:55:01 +0000

    cleanup jogamp move

--- examples/javaonedemo/nbody.bat
+++ examples/javaonedemo/nbody.bat
@@ -1,7 +1,7 @@
 @echo off
 java ^
-  -Djava.library.path=..\..\com.amd.aparapi.jni\dist;jogamp\windows-%PROCESSOR_ARCHITECTURE% ^
-  -classpath jogamp\gluegen-rt.jar;jogamp\jogl-all.jar;..\..\com.amd.aparapi\dist\aparapi.jar;javaonedemo.jar ^
+  -Djava.library.path=..\..\com.amd.aparapi.jni\dist;..\third-party\jogamp\windows-%PROCESSOR_ARCHITECTURE% ^
+  -classpath ..\third-party\jogamp\gluegen-rt.jar;..\third-party\jogamp\jogl-all.jar;..\..\com.amd.aparapi\dist\aparapi.jar;javaonedemo.jar ^
   com.amd.aparapi.examples.javaonedemo.NBody 
 
 
--- examples/javaonedemo/nbody.sh
+++ examples/javaonedemo/nbody.sh
@@ -1,12 +1,4 @@
 #!/bin/bash 
-case $(uname -i) in 
-    x86_64 )
-        export ARCH=linux-amd64 ;;
-    *)
-        export ARCH=linux-i586 ;;
-esac
-
-java \
-  -Djava.library.path=../../com.amd.aparapi.jni/dist:jogamp/${ARCH}  \
-  -classpath jogamp/gluegen-rt.jar:jogamp/jogl-all.jar:../../com.amd.aparapi/dist/aparapi.jar:javaonedemo.jar \
+java -Djava.library.path=../../com.amd.aparapi.jni/dist:../third-party/jogamp  \
+  -classpath ../third-party/jogamp/gluegen-rt.jar:../third-party/jogamp/jogl-all.jar:../../com.amd.aparapi/dist/aparapi.jar:javaonedemo.jar \
   com.amd.aparapi.examples.javaonedemo.NBody 
