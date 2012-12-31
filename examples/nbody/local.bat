1a23b692530cc0f6c3e0f2ad762ebee326b801a3
Gary Frost <frost.gary@gmail.com>
2012-12-31 17:37:06 +0000

    moved multiple copies of jogamp to single dir - third-party and changed build and run scripts to refer to this one dir

--- examples/nbody/local.bat
+++ examples/nbody/local.bat
@@ -1,7 +1,7 @@
 @echo off
 
 java ^
-  -Djava.library.path=..\..\com.amd.aparapi.jni\dist;jogamp ^
+  -Djava.library.path=..\..\com.amd.aparapi.jni\dist;..\third-party\jogamp ^
   -Dcom.amd.aparapi.executionMode=%1 ^
   -Dcom.amd.aparapi.enableShowGeneratedOpenCL=true ^
   -Dcom.amd.aparapi.enableVerboseJNI=false ^
@@ -9,7 +9,7 @@ java ^
   -Dbodies=%2 ^
   -Dheight=600 ^
   -Dwidth=600 ^
-  -classpath jogamp\gluegen-rt.jar;jogamp\jogl.all.jar;..\..\com.amd.aparapi\dist\aparapi.jar;nbody.jar ^
+  -classpath ..\third-party\jogamp\gluegen-rt.jar;..\third-party\jogamp\jogl.all.jar;..\..\com.amd.aparapi\dist\aparapi.jar;nbody.jar ^
   com.amd.aparapi.examples.nbody.Local
 
 
--- examples/nbody/nbody.bat
+++ examples/nbody/nbody.bat
@@ -1,14 +1,14 @@
 @echo off
 
 java ^
-  -Djava.library.path=..\..\com.amd.aparapi.jni\dist;jogamp ^
+  -Djava.library.path=..\..\com.amd.aparapi.jni\dist;..\third-party\jogamp ^
   -Dcom.amd.aparapi.executionMode=%1 ^
   -Dcom.amd.aparapi.enableProfiling=false ^
   -Dcom.amd.aparapi.enableShowGeneratedOpenCL=true ^
   -Dbodies=%2 ^
   -Dheight=600 ^
   -Dwidth=600 ^
-  -classpath jogamp\gluegen-rt.jar;jogamp\jogl-all.jar;..\..\com.amd.aparapi\dist\aparapi.jar;nbody.jar ^
+  -classpath ..\third-party\jogamp\gluegen-rt.jar;..\third-party\jogamp\jogl-all.jar;..\..\com.amd.aparapi\dist\aparapi.jar;nbody.jar ^
   com.amd.aparapi.examples.nbody.Main 
 
 
