a59b55bb43ba37c506f35fcbbc6bc6bc060db2a3
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2018-04-19 11:58:12 -0400

    test: bug in how unit test loads resource.
    ISSUES CLOSED: #89

--- src/test/java/com/aparapi/runtime/LoadClTest.java
+++ src/test/java/com/aparapi/runtime/LoadClTest.java
@@ -28,7 +28,6 @@ import static org.junit.Assert.assertTrue;
 
 public class LoadClTest {
 
-    @Ignore("Ported over but not working yet")
     @Test
     public void test() {
         final int size = 32;
@@ -62,10 +61,10 @@ public class LoadClTest {
         }
     }
 
-    @Resource("com/aparapi/codegen/test/runtime/squarer.cl")
+    @Resource("squarer.cl")
     interface Squarer extends OpenCL<Squarer> {
-        public Squarer square(//
-                              Range _range,//
+        public Squarer square(
+                              Range _range,
                               @GlobalReadWrite("in") float[] in,//
                               @GlobalReadWrite("out") float[] out);
     }
