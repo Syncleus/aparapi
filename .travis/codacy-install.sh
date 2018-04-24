f1b7645559bc9adb842f8139a1866f8af75d3720
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2018-04-23 20:33:19 -0400

    ci(travis): fixes issue where travis fails to get codacy depenency.
    ISSUES CLOSED: #90

---
+++ .travis/codacy-install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+sudo apt-get install libxml-xpath-perl
+# get latest version of codacy reporter from sonatype
+latest=$(curl "https://oss.sonatype.org/service/local/repositories/releases/content/com/codacy/codacy-coverage-reporter/maven-metadata.xml" | xpath -e "/metadata/versioning/release/text()")
+
+echo Downloading latest version $latest of codacy reporter from sonatype
+# download laterst assembly jar
+mvn dependency:get dependency:copy \
+   -DoutputDirectory=$HOME \
+   -DoutputAbsoluteArtifactFilename=true \
+   -Dmdep.stripVersion=true \
+   -DrepoUrl=https://oss.sonatype.org/service/local/repositories/releases/content/ \
+   -Dartifact=com.codacy:codacy-coverage-reporter:$latest:jar:assembly
