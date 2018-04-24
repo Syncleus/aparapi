f6db32afec8b5d3cc3728b707338ef15a191e7bd
CoreRasurae <luis.p.mendes@gmail.com>
2018-04-24 22:41:37 +0100

    Updated tests that were ocasionaly giving timeout, created test for issue #81 NegativeIntegerTest

---
+++ src/test/java/com/aparapi/runtime/NegativeIntegerTest.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) 2016 - 2017 Syncleus, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aparapi.runtime;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assume.assumeTrue;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import com.aparapi.Kernel;
+import com.aparapi.Range;
+import com.aparapi.device.Device;
+import com.aparapi.device.OpenCLDevice;
+import com.aparapi.internal.kernel.KernelManager;
+
+/**
+ * Originally created for testing issue #81
+ *
+ */
+public class NegativeIntegerTest
+{
+	
+    private static OpenCLDevice openCLDevice = null;
+
+    private class CLKernelManager extends KernelManager {
+    	@Override
+    	protected List<Device.TYPE> getPreferredDeviceTypes() {
+    		return Arrays.asList(Device.TYPE.ACC, Device.TYPE.GPU, Device.TYPE.CPU);
+    	}
+    }
+    
+    @Before
+    public void setUpBeforeClass() throws Exception {
+    	KernelManager.setKernelManager(new CLKernelManager());
+        Device device = KernelManager.instance().bestDevice();
+        if (device == null || !(device instanceof OpenCLDevice)) {
+        	System.out.println("!!!No OpenCLDevice available for running the integration test");
+        }
+        assumeTrue (device != null && device instanceof OpenCLDevice);
+        openCLDevice = (OpenCLDevice) device;
+    }
+    
+    @Ignore("Test currently failing")
+    @Test
+    public void negativeIntegerTestPass()
+    {
+    	final Device device = openCLDevice;
+        final int SIZE = 1;
+        final int[] RESULT = new int[2];
+        Kernel kernel = new Kernel()
+        {
+             @Override
+            public void run()
+            {
+                RESULT[0] = -800;
+            }
+        };
+        kernel.execute(Range.create(device, SIZE, SIZE));
+        assertEquals("Result doesn't match", -800, RESULT[0]);
+    }
+}
