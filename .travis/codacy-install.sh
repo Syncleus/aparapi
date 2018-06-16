8dddc685fe6c8315e94990e32781576c873c0075
Pr0methean <4961925+Pr0methean@users.noreply.github.com>
2018-06-15 18:56:46 -0700

    Work around Xpath OSX issue

--- .travis/codacy-install.sh
+++ .travis/codacy-install.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
-# get latest version of codacy reporter from sonatype
-latest=$(curl "https://oss.sonatype.org/service/local/repositories/releases/content/com/codacy/codacy-coverage-reporter/maven-metadata.xml" | xpath -e "/metadata/versioning/release/text()")
+if [[ ${TRAVIS_OS_NAME} == "osx" ]]; then
+  # Xpath doesn't work on OSX, so use a hard-coded version number
+  latest="4.0.1"
+else
+  # get latest version of codacy reporter from sonatype
+  latest=$(curl "https://oss.sonatype.org/service/local/repositories/releases/content/com/codacy/codacy-coverage-reporter/maven-metadata.xml" | xpath -e "/metadata/versioning/release/text()")
+fi
 
 echo Downloading latest version $latest of codacy reporter from sonatype
 # download laterst assembly jar
